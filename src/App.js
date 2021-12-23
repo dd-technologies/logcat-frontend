22c66bb0c589e89ef7b389707627c5c622e22374
Neeraj Singh <69689337+neerajsingh710@users.noreply.github.com>
2021-12-23 23:52:57 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
-import './App.scss';
-import Home from './components/project/home/Home';
+import logo from "./logo.svg";
+import "./App.scss";
+import Home from "./components/project/home/Home";
+import CreateProject from "./components/project/home/Create_project/CreateProject";
 
 function App() {
   return (
     <>
-      <Home />
+      {/* <Home /> */}
+      <CreateProject />
     </>
-
-
   );
 }
 
--- src/components/navbar/Sidebar/SideNav.jsx
+++ src/components/navbar/Sidebar/SideNav.jsx
@@ -1,24 +1,46 @@
 import React from "react";
 import Style from "./SideNav.module.scss";
-import { Row } from "react-bootstrap";
+import { Image, Row, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell, faHome, faCog } from "@fortawesome/free-solid-svg-icons";
 
 export default function SideNav() {
   return (
     <>
-      <Row className={Style.SideNav}>
-        {/* companoy logo */}
-        <section className={Style.Logo}>
-          <img src="/assets/images/DD.png" />
-        </section>
+      <section className={Style.SideNav}>
+        <Row>
+          <Col xl={12}>
+            <section className={Style.componysec}>
+              {/* companoy logo */}
+              <Image src="/assets/images/DD.png" />
+              <p>D&D technology</p>
+            </section>
+          </Col>
+        </Row>
 
-        <section className={Style.SideNavMain}>
-          <p>Project</p>
-          <section>
-            <p>PorjectName</p>
-            <p>Settings</p>
-          </section>
-        </section>
-      </Row>
+        {/* project section */}
+
+        <Row className={`${Style.projectSection} mt-4`}>
+          <Col xl={12}>
+            <Row cla>
+              <Col xl={12} className={`${Style.projectSectionCardOuter} mt-4`}>
+                <FontAwesomeIcon icon={faHome} />
+                <p>Project</p>
+                <FontAwesomeIcon icon={faCog} />
+              </Col>
+              <Col xl={12} className={Style.projectSectionCard}>
+                <FontAwesomeIcon icon={faHome} />
+                <p>Project</p>
+              </Col>
+              <Col xl={12} className={Style.projectSectionCard}>
+                <FontAwesomeIcon icon={faHome} />
+                <p>Project</p>
+              </Col>
+            </Row>
+          </Col>
+          {/* project details */}
+        </Row>
+      </section>
     </>
   );
 }
