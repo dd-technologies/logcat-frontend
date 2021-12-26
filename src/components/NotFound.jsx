fa43606bc17f31141bf4ad0ff67b800cca476f85
neerajsinghdd2 <neerajsinghdd2@gmail.com>
2021-12-26 15:26:49 +0530

    updated code with some ui changes and shaktish code merge done

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -8,24 +8,18 @@ export default function NotFound() {
     <>
       <section className={Style.page_404}>
         <div className={Style.container}>
-          <div className={Style.row}>
-            <div className="col-sm-12">
-              <div className="col-sm-10 col-sm-offset-1  text-center">
-                <div className={Style.four_zero_four_bg}>
-                  <h1 className="text-center">404</h1>
-                </div>
+          <div className={Style.four_zero_four_bg}>
+            <h1 className="text-center">404</h1>
+          </div>
 
-                <div className={Style.contant_box_404}>
-                  <h3 className={Style.h2}>Look like you're lost</h3>
+          <div className={Style.contant_box_404}>
+            <h3 className={Style.h2}>Look like you're lost</h3>
 
-                  <p>the page you are looking for not avaible!</p>
+            <p>the page you are looking for not avaible!</p>
 
-                  <Link to="/">
-                    <Button>Go to Home</Button>
-                  </Link>
-                </div>
-              </div>
-            </div>
+            <Link to="/">
+              <Button className="mt-2">Go to Home</Button>
+            </Link>
           </div>
         </div>
       </section>
