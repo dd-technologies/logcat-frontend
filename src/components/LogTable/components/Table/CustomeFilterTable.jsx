a6bc529eb0277500f697f9ab0ddaa2f0907f254f
neerajsingdd2 <neerajsinghdd2@gmail.com>
2021-12-28 17:53:04 +0530

    data filter with table created

---
+++ src/components/LogTable/components/Table/CustomeFilterTable.jsx
@@ -0,0 +1,133 @@
+import React, { useState } from "react";
+import { Button, Col, Row } from "react-bootstrap";
+import CustomCard from "../../../../Container/CustomCard";
+import Style from "./CustomeFilterTable.module.scss";
+
+export default function CustomeFilterTable() {
+  const [dateSectionSelect, setDateSectionSelect] = useState(true);
+  const [StatusSectionSeclect, setStatusSectionSeclect] = useState(false);
+  const [countPerPageSection, setCountPerPageSection] = useState(false);
+
+  // SHOW DATE SECTION FUNCTION
+  const handleShowDate = () => {
+    setDateSectionSelect(true);
+    setStatusSectionSeclect(false);
+    setCountPerPageSection(false);
+  };
+  // SHOW STATUS CODE SECTION FUNCTION
+  const handleShowStatus = () => {
+    setDateSectionSelect(false);
+    setStatusSectionSeclect(true);
+    setCountPerPageSection(false);
+  };
+
+  // SHOW PAGE PER COUNT SECTION FUNCTION
+  const handleShowPerPage = () => {
+    setDateSectionSelect(false);
+    setStatusSectionSeclect(false);
+    setCountPerPageSection(true);
+  };
+
+  return (
+    <>
+      <CustomCard
+        position="absolute"
+        height="auto"
+        width="450px"
+        right="2%"
+        padding="10px"
+        boxShadow="0px 0px 4px -2px rgba(0,0,0,0.75)"
+      >
+        <section className={Style.TopButton}>
+          <Button className="m-2">Reset Filter</Button>
+          <Button className="m-2">Save Filter</Button>
+          <Button className="m-2">Apply Filter</Button>
+        </section>
+        <section>
+          <Row>
+            <Col xl={6}>
+              <section className="m-2">
+                <p
+                  className={
+                    dateSectionSelect
+                      ? `${Style.ActiveOption} mt-2`
+                      : `${Style.DefaultOption} mt-2`
+                  }
+                  onClick={handleShowDate}
+                >
+                  Date
+                </p>
+                <p
+                  className={
+                    StatusSectionSeclect
+                      ? `${Style.ActiveOption} mt-2`
+                      : `${Style.DefaultOption} mt-2`
+                  }
+                  onClick={handleShowStatus}
+                >
+                  Select an option
+                </p>
+                <p
+                  className={
+                    countPerPageSection
+                      ? `${Style.ActiveOption} mt-2`
+                      : `${Style.DefaultOption} mt-2`
+                  }
+                  onClick={handleShowPerPage}
+                >
+                  Date per page
+                </p>
+              </section>
+            </Col>
+
+            {/* DATA CHANGE SECTION START FROM HERE */}
+            {dateSectionSelect ? (
+              <Col xl={6}>
+                <section className={Style.DateSection}>
+                  <input type="date" />
+                  <input type="date" />
+                </section>
+              </Col>
+            ) : null}
+
+            {/* STATUS CODE SECTION START HERE */}
+            {StatusSectionSeclect ? (
+              <Col xl={6}>
+                <section className={Style.StatusSection}>
+                  <section className={Style.StatusInnerSecion}>
+                    <label for="exampleFormControlFile1">Info</label>
+                    <input type="checkbox" />
+                  </section>
+                  <section className={Style.StatusInnerSecion}>
+                    <label for="exampleFormControlFile1">Worm</label>
+                    <input type="checkbox" />
+                  </section>
+                  <section className={Style.StatusInnerSecion}>
+                    <label for="exampleFormControlFile1">Error</label>
+                    <input type="checkbox" />
+                  </section>
+                  <section className={Style.StatusInnerSecion}>
+                    <label for="exampleFormControlFile1">Debug</label>
+                    <input type="checkbox" />
+                  </section>
+                </section>
+              </Col>
+            ) : null}
+
+            {/* COUNT PER PAGE SECTION START FOM HERE   */}
+            {countPerPageSection ? (
+              <Col xl={6}>
+                <section className={Style.perPageOuter}>
+                  <p className={Style.perPagesectionInner}>10</p>
+                  <p className={Style.perPagesectionInner}>25</p>
+                  <p className={Style.perPagesectionInner}>45</p>
+                  <p className={Style.perPagesectionInner}>50</p>
+                </section>
+              </Col>
+            ) : null}
+          </Row>
+        </section>
+      </CustomCard>
+    </>
+  );
+}
