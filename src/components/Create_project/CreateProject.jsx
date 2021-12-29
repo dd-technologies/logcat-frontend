17795d80fb00e175f553824d40db1c20bf663533
neerajsingdd2 <neerajsinghdd2@gmail.com>
2021-12-29 14:52:02 +0530

    updated code with project nav with logut and host na

--- src/components/Create_project/CreateProject.jsx
+++ src/components/Create_project/CreateProject.jsx
@@ -31,16 +31,23 @@ function CreateProject() {
   const getAllProjectReducer = useSelector(
     (state) => state.getAllProjectReducer
   );
-  const { loading, allProjectData } = getAllProjectReducer;
+  const {
+    allProjectData: PorjectData,
+    loading,
+    allProjectData,
+  } = getAllProjectReducer;
+  console.log("PorjectData", PorjectData);
 
+  // GETTGIN THE USER NAME TO PUT IN DOCUMENT
   const adminLoginReducer = useSelector((state) => state.adminLoginReducer);
   const { adminInfo } = adminLoginReducer;
+
+  console.log("adminInfo", adminInfo);
 
   const createNewProjectReducer = useSelector(
     (state) => state.createNewProjectReducer
   );
   const { data } = createNewProjectReducer;
-  // console.log("data", allProjectData);
   if (data && data.data) {
     toast.success("Project Created Successfully");
     Dispatch(clearProjectData());
@@ -77,42 +84,52 @@ function CreateProject() {
   return (
     <>
       {/* user name with logout functionalty */}
-      <>
-        <section className={Style.backgroundSection}></section>
-        <Container className={Style.MainContantainer}>
-          <p
-            className={Style.logout}
-            onClick={(e) => {
-              handlelogout(e);
-            }}
-          >
-            Logout
-          </p>
-          <p className={Style.para}>Your Projects</p>
-          <Row>
-            <Col xl={4} lg={4} md={6} sm={6} className="mt-4">
-              <CustomCard padding="10px" height="200px">
-                <section
-                  className={Style.addProject}
-                  onClick={() => setModalShow(true)}
+      {PorjectData && PorjectData.data && PorjectData.data.data ? (
+        <>
+          <section className={Style.backgroundSection}></section>
+          <Container className={Style.MainContantainer}>
+            <Row>
+              <Col
+                xl={12}
+                className="d-flex justify-content-end align-items-center"
+              >
+                <p className="px-2">{adminInfo.data.name}</p>
+                <Button
+                  onClick={(e) => {
+                    handlelogout(e);
+                  }}
                 >
-                  <section>
-                    <p>
-                      <FontAwesomeIcon icon={faPlus} />
-                    </p>
-                    <p>Add Project</p>
+                  Logout
+                </Button>
+              </Col>
+            </Row>
+            <Row>
+              <Col xl={12}>
+                <p className={Style.para}>Your Projects</p>
+              </Col>
+              <Col xl={4} lg={4} md={6} sm={6} className="mt-4">
+                <CustomCard padding="10px" height="200px">
+                  <section
+                    className={Style.addProject}
+                    onClick={() => setModalShow(true)}
+                  >
+                    <section>
+                      <p>
+                        <FontAwesomeIcon icon={faPlus} />
+                      </p>
+                      <p>Add Project</p>
+                    </section>
                   </section>
-                </section>
-              </CustomCard>
-              <AddProjectModal
-                show={modalShow}
-                onHide={() => setModalShow(false)}
-              />
-            </Col>
+                </CustomCard>
+                <AddProjectModal
+                  show={modalShow}
+                  onHide={() => setModalShow(false)}
+                />
+              </Col>
 
-            {/* dynamic projects */}
+              {/* dynamic projects */}
 
-            {/* <Col xl={4} lg={4} md={6} sm={6} className="mt-4">
+              {/* <Col xl={4} lg={4} md={6} sm={6} className="mt-4">
             <CustomCard padding="10px">
               <Row>
                 <Col xl={12} className={Style.InfoColumn}>
@@ -133,14 +150,17 @@ function CreateProject() {
               </Row>
             </CustomCard>
           </Col> */}
-            {allProjectData &&
-              allProjectData.data.data.length &&
-              allProjectData.data.data.map((datas) => (
-                <ProjectCard data={datas} />
-              ))}
-          </Row>
-        </Container>
-      </>
+              {allProjectData &&
+                allProjectData.data.data.length &&
+                allProjectData.data.data.map((datas) => (
+                  <ProjectCard data={datas} />
+                ))}
+            </Row>
+          </Container>
+        </>
+      ) : (
+        <Spinner />
+      )}
       )
     </>
   );
