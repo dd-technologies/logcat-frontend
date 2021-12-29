daeac3a49fa79ede9bdaaa3517232db7dfa073fd
neerajsingdd2 <neerajsinghdd2@gmail.com>
2021-12-29 16:42:01 +0530

    updated code witl analytics filter text button

--- src/components/Analytics/Componets/StackData.jsx
+++ src/components/Analytics/Componets/StackData.jsx
@@ -2,11 +2,14 @@ import React, { useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import Style from "./StackData.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import { faCaretDown, faFilter } from "@fortawesome/free-solid-svg-icons";
 
 export default function StackData() {
   const [InnerParaShow, setInnerParaShow] = useState(false);
   const [innerParaShowDetails, setInnerParaShowDetails] = useState(true);
+  const [stackErrorFilter, setStackErrorFilter] = useState(true);
+  const [stackErrorFilterTextFormate, setStackErrorFilterTextFormate] =
+    useState(false);
 
   // GETTGIN DATA FROM URL
   const queryString = window.location.search;
@@ -36,30 +39,70 @@ export default function StackData() {
     setInnerParaShowDetails(!innerParaShowDetails);
   };
 
+  const stackErrorFilterFun = () => {
+    setStackErrorFilter(true);
+    setStackErrorFilterTextFormate(false);
+  };
+
+  const stackErrorFilterTextFormateFun = () => {
+    setStackErrorFilterTextFormate(true);
+    setStackErrorFilter(false);
+  };
+
   return (
     <>
       <Row className="p-4">
-        <Col xl={12} className={`${Style.outerDiv} mt-4`}>
-          <section className={`${Style.StackInfoDive} p-3`}>
-            <section className={Style.ParaWithArrowDown}>
-              <h3 onClick={innerParaShowFun}>Stack Error</h3>
-              <FontAwesomeIcon icon={faCaretDown} />
+        <Col xl={12}>
+          {/* TEXT AND COE TOGGLE SECTION */}
+          <section className={Style.filterToggle}>
+            <section
+              className={Style.filterGraphFirstSction}
+              onClick={stackErrorFilterFun}
+            >
+              <FontAwesomeIcon icon={faFilter} />
             </section>
-            {InnerParaShow ? <p>{colData}</p> : null}
-          </section>
-          <section className={`${Style.detailSection} p-3`}>
-            <FontAwesomeIcon icon={faCaretDown} />
-            <section className={`${Style.lineBrack} ps-4`}>
-              <p onClick={innerParaShowDetailsFun}>Errors Row</p>
-
-              {innerParaShowDetails ? (
-                <section>
-                  <p>{DataINRow}</p>
-                </section>
-              ) : null}
+            <section
+              className={Style.filterGraphFirstSction}
+              onClick={stackErrorFilterTextFormateFun}
+            >
+              <p>Text</p>
             </section>
           </section>
         </Col>
+
+        {/* STACK ERRO WITH  FILLTER BUTTON CLICK */}
+        {stackErrorFilter ? (
+          <Col xl={12} className={`${Style.outerDiv} mt-4`}>
+            <section className={`${Style.StackInfoDive} p-3`}>
+              <section className={Style.ParaWithArrowDown}>
+                <h3 onClick={innerParaShowFun}>Stack Error</h3>
+                <FontAwesomeIcon icon={faCaretDown} />
+              </section>
+            </section>
+
+            <section className={`${Style.detailSection} p-3`}>
+              <p className={Style.stackDetails}>Stack Error</p>
+              {InnerParaShow ? <p>{colData}</p> : null}
+            </section>
+          </Col>
+        ) : null}
+
+        {/* TEXT FILTER STACK ERROR FIELD HERE */}
+        {stackErrorFilterTextFormate ? (
+          <Col xl={12} className={`${Style.outerDiv} mt-4`}>
+            <section className={`${Style.StackInfoDive} p-3`}>
+              <section className={Style.ParaWithArrowDown}>
+                <h3 onClick={innerParaShowFun}>Stack Error in text formate</h3>
+                <FontAwesomeIcon icon={faCaretDown} />
+              </section>
+            </section>
+
+            <section className={`${Style.detailSection} p-3`}>
+              <p className={Style.stackDetails}>Stack Error</p>
+              {InnerParaShow ? <p>{colData}</p> : null}
+            </section>
+          </Col>
+        ) : null}
       </Row>
     </>
   );
