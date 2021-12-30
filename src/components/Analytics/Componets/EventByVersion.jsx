55f22051c308cf909f1ad84555b0aff643f65cea
ShaktishPrajapati <pshaktish1995@gmail.com>
2021-12-30 17:12:20 +0530

    api integration verion model

--- src/components/Analytics/Componets/EventByVersion.jsx
+++ src/components/Analytics/Componets/EventByVersion.jsx
@@ -2,16 +2,31 @@ import React, { useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 import CustomCard from "../../../Container/CustomCard";
 import EventByVersionChart from "../charts/EventByVersionChart";
+import { useSelector } from "react-redux";
 
 export default function EventByVersion() {
+  const getCrashAnalyticsDataReducer = useSelector(state => state.getCrashAnalyticsDataReducer)
+  const {loading:ld,data :alldata} = getCrashAnalyticsDataReducer;
+  const cnt = alldata && alldata.versionResponse ? alldata.versionResponse : null;
+  let adds=0;
+  if (cnt) {
+    cnt.map(e=>adds+=e.countLog);
+  }
+  console.log(alldata)
   return (
     <>
       <CustomCard>
         <Row className="p-4">
           <Col xl={4}>
             <p>Total events by version</p>
-            <p>2.35.4.55</p>
-            <h4>5</h4>
+            {
+              !ld ? cnt.map(e=>(<>
+                <p>{e._id}</p>
+              <h4>{e.countLog}</h4>
+              </>
+              )):'Loading'
+            }
+            
           </Col>
           <Col xl={8}>
             <section>
