1c83855942e3feaeb07b82fa9540a48774f2959c
neerajdd2 <neerajsinghdd2@gmail.com>
2022-04-16 18:03:09 +0530

    updated code with UI fixes

--- src/utils/ThemeContext.jsx
+++ src/utils/ThemeContext.jsx
@@ -2,11 +2,17 @@ import { createContext } from "react";
 
 // implementing dark theme light theme by context api
 export const themes = {
-  dark: "",
-  light: "white-content",
+  dark: "dark-content",
+  light: "",
+};
+
+export const sidebarTheme = {
+  sideBar: "sidebar",
+  noSideBar: "",
 };
 
 export const ThemeContext = createContext({
   theme: themes.dark,
   changeTheme: () => {},
+  sideBar: () => {},
 });
--- src/utils/ThemeContextWrapper.jsx
+++ src/utils/ThemeContextWrapper.jsx
@@ -1,29 +1,55 @@
 import React, { useState, useEffect } from "react";
-import { ThemeContext, themes } from "./ThemeContext";
+import { ThemeContext, themes, sidebarTheme } from "./ThemeContext";
 
 export default function ThemeContextWrapper(props) {
   const [theme, setTheme] = useState(themes.dark);
+  const [sidebar, setSideBar] = useState(sidebarTheme.sidebarVisibility);
 
+  // change theme
   function changeTheme(theme) {
     setTheme(theme);
+  }
+
+  // change sidebar
+  function sidebarTheme(sideBar) {
+    setSideBar(sideBar);
   }
 
   // console.log("first 12", document.body);
 
   useEffect(() => {
     switch (theme) {
-      case themes.light:
+      case themes.dark:
         document.body.classList.add("dark-content");
         break;
-      case themes.dark:
+      case themes.light:
       default:
         document.body.classList.remove("dark-content");
         break;
     }
   }, [theme]);
 
+  // SIDEBAR CHANGE
+  useEffect(() => {
+    switch (sidebar) {
+      case sidebarTheme.sidebarVisibility:
+        document.body.classList.add("sidebar");
+        break;
+      case themes.noSideBar:
+      default:
+        document.body.classList.remove("sidebar");
+        break;
+    }
+  }, [sidebar]);
+
   return (
-    <ThemeContext.Provider value={{ theme: theme, changeTheme: changeTheme }}>
+    <ThemeContext.Provider
+      value={{
+        theme: theme,
+        changeTheme: changeTheme,
+        sidebarTheme: sidebarTheme,
+      }}
+    >
       {props.children}
     </ThemeContext.Provider>
   );
