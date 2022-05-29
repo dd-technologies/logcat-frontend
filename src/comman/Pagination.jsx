f762887ca9a4c169df68067d421d1b9306371cab
neerajsingh-agva <neerajsingh@ddtech.in>
2022-05-29 18:03:09 +0530

    test push

---
+++ src/comman/Pagination.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { getProjectByCode } from "../redux/action/ProjectAction";
+import ReactPaginate from "react-paginate";
+
+export default function Pagination({
+  data,
+  record,
+  handlePageClick,
+}) {
+  return (
+    <ReactPaginate
+      breakLabel=". . ."
+      nextLabel="Next >"
+      onPageChange={handlePageClick}
+      pageRangeDisplayed={4}
+      pageCount={Math.ceil(data / record)}
+      renderOnZeroPageCount={null}
+      containerClassName={"pagination"}
+      pageClassName={"page-item"}
+      pageLinkClassName={"page-link"}
+      previousClassName={"page-item"}
+      nextClassName={"page-item"}
+      previousLinkClassName={"page-link"}
+      nextLinkClassName={"page-link"}
+      activeClassName={"active"}
+    />
+  );
+}
